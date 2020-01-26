dcc377cd8204d4cad941996bee2fd9b321db1e14
James Liu <james_liu@fastmail.com>
2020-01-26 16:38:16 +1100

    refactor: move `stdrwc` to `stdio.go`

--- internal/server/server.go
+++ internal/server/server.go
@@ -44,20 +44,3 @@ func Run(cfg Config) error {
 		return fmt.Errorf("invalid mode %q", *cfg.Mode)
 	}
 }
-
-type stdrwc struct{}
-
-func (stdrwc) Read(p []byte) (int, error) {
-	return os.Stdin.Read(p)
-}
-
-func (stdrwc) Write(p []byte) (int, error) {
-	return os.Stdout.Write(p)
-}
-
-func (stdrwc) Close() error {
-	if err := os.Stdin.Close(); err != nil {
-		return err
-	}
-	return os.Stdout.Close()
-}
--- internal/server/stdio.go
+++ internal/server/stdio.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/disposedtrolley/golsp-sdk/internal/handler"
 	"github.com/sourcegraph/jsonrpc2"
@@ -25,4 +26,21 @@ func (s *StdioServer) Serve(connOpts []jsonrpc2.ConnOpt) error {
 
 	log.Println("connection closed")
 	return nil
+}
+
+type stdrwc struct{}
+
+func (stdrwc) Read(p []byte) (int, error) {
+	return os.Stdin.Read(p)
+}
+
+func (stdrwc) Write(p []byte) (int, error) {
+	return os.Stdout.Write(p)
+}
+
+func (stdrwc) Close() error {
+	if err := os.Stdin.Close(); err != nil {
+		return err
+	}
+	return os.Stdout.Close()
 }
